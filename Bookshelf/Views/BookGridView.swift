879722b7bed2b8334cc2ae0809047bd0e560f0b5
adp1906 <66088822+adp1906@users.noreply.github.com>
2024-07-24 17:22:10 -0500

    Add Grid View Toggle

---
+++ Bookshelf/Views/BookGridView.swift
@@ -0,0 +1,43 @@
+//
+//  BookGridView.swift
+//  Bookshelf
+//
+//  Created by Aaron Peterson on 7/24/24.
+//
+
+import SwiftUI
+
+struct BookGridView: View {
+    var books: [Book]
+    var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(books) { book in
+                    if let url = URL(string: book.imageUrl) {
+                        AsyncImage(url: url) { image in
+                            image.resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .cornerRadius(8)
+                        } placeholder: {
+                            ProgressView()
+                        }
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct BookGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookGridView(books: [Book(id: UUID(),
+                                  title: "Sample Book",
+                                  author: "Sample Author",
+                                  imageUrl: "",
+                                  isLoaned: false)])
+    }
+}
